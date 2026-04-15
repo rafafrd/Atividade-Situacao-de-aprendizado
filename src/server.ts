b0bfdb01e7da1f022731d0d290f8e6ea5e9f88df
guimunizzz <guilherme.muniz5@hotmail.com>
2026-04-15 09:20:41 -0300

    feat: config server.ts

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { EnvVar } from "./config/EnvVar";
+import path from 'path';
+import router from "./routes/routes";
+
+const app = express();
+
+app.use(express.json());
+app.use('/', router)
+
+app.use('/produtos', express.static(path.resolve('uploads/Images')));
+
+app.listen(EnvVar.SERVER_PORT, ()=> {
+    console.log(`Servidor rodando em http://localhost:${EnvVar.SERVER_PORT}`)
+})
