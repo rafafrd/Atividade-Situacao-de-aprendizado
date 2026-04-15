633fbf542fb0f8d12667584df5d04c3ef219698e
guimunizzz <guilherme.muniz5@hotmail.com>
2026-04-15 10:39:36 -0300

    fix: fornecedores repository variables

--- src/repository/fornecedores.repository.ts
+++ src/repository/fornecedores.repository.ts
@@ -15,21 +15,21 @@ export class FornecedoresRepository {
         return rows;
     }
     async adicionarFornecedor(dados: Fornecedores): Promise<ResultSetHeader> {
-        const sql = 'INSERT INTO fornecedores (dc_fornecedores) VALUES (?);';
+        const sql = 'INSERT INTO fornecedores (dc_fornecedor) VALUES (?);';
         const values = [dados.DescricaoFornecedor];
         const [rows] = await db.execute<ResultSetHeader>(sql, values);
         return rows;
     }
 
     async editarFornecedor(id: number, dados: Fornecedores): Promise<ResultSetHeader> {
-        const sql = 'UPDATE categorias SET dc_categoria = ? WHERE id_categoria = ?;';
+        const sql = 'UPDATE fornecedores SET dc_fornecedor = ? WHERE id_fornecedor = ?;';
         const values = [dados.DescricaoFornecedor, id]
         const [rows] = await db.execute<ResultSetHeader>(sql, values);
         return rows
     }
 
     async deletarFornecedor(id: number): Promise<ResultSetHeader> {
-        const sql = 'DELETE FROM categorias WHERE id_categoria = ?';
+        const sql = 'DELETE FROM fornecedores WHERE id_fornecedor = ?';
         const values = [id];
         const [rows] = await db.execute<ResultSetHeader>(sql, values);
         return rows;
