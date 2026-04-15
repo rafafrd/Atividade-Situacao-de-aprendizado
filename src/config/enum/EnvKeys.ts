a0815fafd869e2e4b87dc2a7156e551b626a885f
guimunizzz <guilherme.muniz5@hotmail.com>
2026-04-15 09:11:40 -0300

    feat: add config env var

---
+++ src/config/enum/EnvKeys.ts
@@ -0,0 +1,8 @@
+export enum EnvKey {
+    SERVER_PORT = 'SERVER_PORT',
+    DB_PORT = 'DB_PORT',
+    DB_HOST = 'DB_HOST',
+    DB_DATABASE = 'DB_DATABASE',
+    DB_USER = 'DB_USER',
+    DB_PASS = 'DB_PASS'
+}
