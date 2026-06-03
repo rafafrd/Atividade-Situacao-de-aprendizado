d6a831152e6699788ae9b23b517d0b01805c26ad
guimunizzz <guilherme.muniz5@hotmail.com>
2026-06-03 09:55:27 -0300

    feat: protected routes test

--- api/src/routes/estoque.routes.ts
+++ api/src/routes/estoque.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { EstoqueController } from '../controllers/estoque.controller';
+import { AuthMiddleware } from "../middlewares/login.middleware";
+import { Role } from "../config/enum/Roles";
 
 const estoqueRouter = Router();
+const authMiddleware = new AuthMiddleware();
 const estoqueController = new EstoqueController();
 
-estoqueRouter.get('/', estoqueController.listarTodos);
-estoqueRouter.get('/report', estoqueController.relatorioEstoque);
-estoqueRouter.get('/:id', estoqueController.buscarPorId);
-estoqueRouter.post('/', estoqueController.criarEstoque);
-estoqueRouter.put('/:id', estoqueController.atualizarEstoque);
-estoqueRouter.delete('/:id', estoqueController.deletarEstoque);
+estoqueRouter.get('/', authMiddleware.authenticate, estoqueController.listarTodos);
+estoqueRouter.get('/report', authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN, Role.USER), estoqueController.relatorioEstoque);
+estoqueRouter.get('/:id', authMiddleware.authenticate, estoqueController.buscarPorId);
+estoqueRouter.post('/', authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN), estoqueController.criarEstoque);
+estoqueRouter.put('/:id', authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN), estoqueController.atualizarEstoque);
+estoqueRouter.delete('/:id', authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN), estoqueController.deletarEstoque);
 
 export default estoqueRouter;
--- api/src/routes/fornecedores.routes.ts
+++ api/src/routes/fornecedores.routes.ts
@@ -1,20 +1,23 @@
 import { Router } from "express";
 import { FornecedoresController } from "../controllers/fornecedores.controller";
+import { AuthMiddleware } from "../middlewares/login.middleware";
+import { Role } from "../config/enum/Roles";
 
 const fornecedoresRouter = Router();
+const authMiddleware = new AuthMiddleware();
 const fornecedoresController = new FornecedoresController();
 
 //get
-fornecedoresRouter.get("/", fornecedoresController.listarTodos);
-fornecedoresRouter.get("/:id", fornecedoresController.buscarPorId); //criar rota para buscar por id
+fornecedoresRouter.get("/", authMiddleware.authenticate, authMiddleware.authorize(Role.USER, Role.MANAGER, Role.ADMIN), fornecedoresController.listarTodos);
+fornecedoresRouter.get("/:id", authMiddleware.authenticate, authMiddleware.authorize(Role.USER, Role.MANAGER, Role.ADMIN), fornecedoresController.buscarPorId); //criar rota para buscar por id
 
 //post
-fornecedoresRouter.post("/", fornecedoresController.criarFornecedor);
+fornecedoresRouter.post("/", authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN),  fornecedoresController.criarFornecedor);
 
 //put
-fornecedoresRouter.put("/:id", fornecedoresController.atualizarFornecedor);
+fornecedoresRouter.put("/:id", authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN), fornecedoresController.atualizarFornecedor);
 
 //delete
-fornecedoresRouter.delete("/:id", fornecedoresController.deletarFornecedor);
+fornecedoresRouter.delete("/:id", authMiddleware.authenticate, authMiddleware.authorize(Role.MANAGER, Role.ADMIN), fornecedoresController.deletarFornecedor);
 
 export default fornecedoresRouter;
