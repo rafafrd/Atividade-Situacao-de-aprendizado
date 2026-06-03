63c89f337668d00597dfe5c797dffc8e50579444
guimunizzz <guilherme.muniz5@hotmail.com>
2026-06-03 09:28:03 -0300

    feat: login controller and fix repository

--- api/src/repository/login.repository.ts
+++ api/src/repository/login.repository.ts
@@ -1,6 +1,6 @@
 import { db } from "../database/connection.database";
 import { ILogin } from "../models/login.model";
-import { ResultSetHeader } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 
 export class LoginRepository {
   async selectTodos(): Promise<ResultSetHeader> {
@@ -8,34 +8,39 @@ export class LoginRepository {
     const [rows] = await db.execute<ResultSetHeader>(sql);
     return rows;
   }
-
-  async findByUsername(dados: ILogin): Promise<ResultSetHeader> {
-    const sql =
-      "SELECT * FROM login WHERE username = ? AND is_active = 1 LIMIT 1;";
-    const values = [dados.username];
-    const [rows] = await db.execute<ResultSetHeader>(sql, values);
-    return rows;
+  async findByUsername(username: string): Promise<ILogin | null> {
+    const [rows] = await db.execute<RowDataPacket[]>(
+      "SELECT * FROM login WHERE username = ? AND is_active = 1 LIMIT 1;",
+      [username],
+    );
+    return rows.length > 0 ? (rows[0] as ILogin) : null;
   }
 
-  async selectById(id_login: number): Promise<ResultSetHeader> {
-    const sql = "SELECT * FROM login WHERE id_login = ?";
-    const values = [id_login];
-    const [rows] = await db.execute<ResultSetHeader>(sql, values);
-    return rows;
+  async selectById(login_id: number): Promise<ILogin | null> {
+    const [rows] = await db.execute<RowDataPacket[]>(
+      "SELECT * FROM login WHERE login_id = ? LIMIT 1;",
+      [login_id],
+    );
+    return rows.length > 0 ? (rows[0] as ILogin) : null;
   }
 
-  async adicionarUsuario(dados: ILogin): Promise<ResultSetHeader> {
-    const sql =
-      "INSERT INTO login (username, password_hash, role) VALUES (?, ?, ?);";
-    const values = [dados.username, dados.password_hash, dados.role];
-    const [rows] = await db.execute<ResultSetHeader>(sql, values);
-    return rows;
+  async adicionarUsuario(
+    username: string,
+    password_hash: string,
+    role: string,
+  ): Promise<number> {
+    const [result] = await db.execute<ResultSetHeader>(
+      "INSERT INTO login (username, password_hash, role) VALUES (?, ?, ?);",
+      [username, password_hash, role],
+    );
+    return result.insertId;
   }
 
-  async deletarUsuario(id_login: number): Promise<ResultSetHeader> {
-    const sql = "DELETE FROM login WHERE id_login = ?";
-    const values = [id_login];
-    const [rows] = await db.execute<ResultSetHeader>(sql, values);
-    return rows;
+  async deletarUsuario(login_id: number): Promise<number> {
+    const [result] = await db.execute<ResultSetHeader>(
+      "DELETE FROM login WHERE login_id = ?;",
+      [login_id],
+    );
+    return result.affectedRows;
   }
 }
